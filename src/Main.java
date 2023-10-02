d3b240404b2d31d8c3369c4842e8a1e51eb926ed
Lisa <lpircher@student.tgm.ac.at>
2023-10-02 20:36:56 +0200

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
         ImageIcon icon = new ImageIcon(new URL("https://i.pinimg.com/originals/c5/17/da/c517da1b29ac7d142f40f489fb9d32ce.jpg"));
         icon.setImage(icon.getImage().getScaledInstance(250,250, Image.SCALE_DEFAULT));
-
         JPanel panel = new JPanel();
         JLabel bild = new JLabel(icon);
         panel.add(bild);
